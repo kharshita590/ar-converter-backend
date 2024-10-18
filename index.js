89f56cbc7ec8c7055db9a7b78151f0bbda9c397a
kharshita590 <83570759+kharshita590@users.noreply.github.com>
2024-10-18 13:01:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 const app = express();
 
 app.options("*", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3003");
+  res.header("Access-Control-Allow-Origin", "https://ar-converter-frontend.vercel.app/");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
@@ -17,7 +17,7 @@ app.options("*", (req, res) => {
 });
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3003");
+  res.header("Access-Control-Allow-Origin", "https://ar-converter-frontend.vercel.app/");
   res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
