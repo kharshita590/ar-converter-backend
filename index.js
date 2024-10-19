1a1bbad67831d619aacb002203ac5d7c20f46f4c
harshitakumai-dev <harshitakumari344@gmail.com>
2024-10-20 00:51:22 +0530

    jo

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 const app = express();
 
 app.options("*", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "https://ar-converter.netlify.app/");
+  res.header("Access-Control-Allow-Origin", "https://ar-converter.netlify.app");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
@@ -17,7 +17,7 @@ app.options("*", (req, res) => {
 });
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://ar-converter.netlify.app/");
+  res.header("Access-Control-Allow-Origin", "https://ar-converter.netlify.app");
   res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
